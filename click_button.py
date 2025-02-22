adb8868b2c7e449ccb9492ee739eb3bff937253a
Navaneeth-Gajula <navaneethgajula17@gmail.com>
2025-02-23 03:02:37 +0530

    trymob

--- click_button.py
+++ click_button.py
@@ -7,6 +7,7 @@ import uuid
 # Set up Chrome options
 options = webdriver.ChromeOptions()
 # Remove the --headless flag
+options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
