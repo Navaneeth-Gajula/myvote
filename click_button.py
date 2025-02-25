f4e44b11efcedd6f46f60c6192b381616278728c
NAVANEETH GAJULA <navaneethgajula17@gmail.com>
2025-02-26 00:55:35 +0530

    Update click_button.py

--- click_button.py
+++ click_button.py
@@ -57,7 +57,7 @@ driver.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument', {
 try:
     # Open the website
     driver.get('https://mycutebaby.in/contest/participant/679e77f65b140')
-    driver.add_cookie({'name': 'PHPSESSID', 'value': '26eb95bd31a9472119020de42fae3af2'})
+    driver.add_cookie({'name': 'PHPSESSID', 'value': '42aa695086fcb9642a6934901c5aaf96'})
     time.sleep(2)
     # to refresh the page
     driver.refresh()
